6519b6cd6c9dada89ca7be82e13b02555d9fa40c
Jorge Ezequiel Diaz <141289604+ProgramEze@users.noreply.github.com>
2026-06-03 01:11:38 -0300

    feat: run migrations on startup and enable Swagger in all environments

--- TaskFlowPro.Api/Program.cs
+++ TaskFlowPro.Api/Program.cs
@@ -53,57 +53,6 @@ builder.Services.AddSwaggerGen(options =>
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-/*
-userId: 50185452-dfe4-4120-9fce-5b172bf78169
-workspaceId: 6b8a1c85-4a22-4a03-a0c4-7a676beb3dff
-projectId: d27fcfc0-3d98-4c46-85e0-e99a020dc6fa
-Tarea 1: a0e99ecd-efb1-4d96-9dbb-a5c1b610e148
-Tarea 2: ca13e46a-58cf-4117-b0d2-28b5c192e1fc
-Tarea 3: 4e501127-548a-4a37-8b5d-8534c8046a87
-Tarea 4: 00a567e5-d84b-438b-ac70-f4d25d1dc560
-Tarea 5: 6d4c6e33-ce68-430f-947c-10c35df6e57f
-Tarea 6: 8be83290-d5aa-44cb-bcdf-f0c2b5291386
-
-{
-  "email": "eze@test.com",
-  "password": "123456"
-}
-
-{
-  "userId": "50185452-dfe4-4120-9fce-5b172bf78169",
-  "firstName": "Ezequiel",
-  "lastName": "Diaz",
-  "email": "eze@test.com",
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjUwMTg1NDUyLWRmZTQtNDEyMC05ZmNlLTViMTcyYmY3ODE2OSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6ImV6ZUB0ZXN0LmNvbSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJFemVxdWllbCBEaWF6IiwiZXhwIjoxNzgwMzQ4MTI5LCJpc3MiOiJUYXNrRmxvd1BybyIsImF1ZCI6IlRhc2tGbG93UHJvVXNlcnMifQ.vlWKUQmP77j9FMFSFo__mwa_P1mj1YgLR-_MtmB4tcE"
-}
-
-{
-  "email": "ana@test.com",
-  "password": "123456"
-}
-
-{
-  "userId": "29549a54-e57f-42fc-a0a1-1a7ea251b705",
-  "firstName": "Ana",
-  "lastName": "Perez",
-  "email": "ana@test.com",
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjI5NTQ5YTU0LWU1N2YtNDJmYy1hMGExLTFhN2VhMjUxYjcwNSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6ImFuYUB0ZXN0LmNvbSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJBbmEgUGVyZXoiLCJleHAiOjE3ODAzNDkyNjQsImlzcyI6IlRhc2tGbG93UHJvIiwiYXVkIjoiVGFza0Zsb3dQcm9Vc2VycyJ9.SS4nqPqUxVXSSVm0oBqfBkayqGw-LD-8rar1bHp1cG0"
-}
-
-{
-  "email": "juan@test.com",
-  "password": "123456"
-}
-
-{
-  "userId": "f1c2a09a-857d-42e7-ac08-7dc9f254e1ce",
-  "firstName": "Juan",
-  "lastName": "Menendez",
-  "email": "juan@test.com",
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImYxYzJhMDlhLTg1N2QtNDJlNy1hYzA4LTdkYzlmMjU0ZTFjZSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6Imp1YW5AdGVzdC5jb20iLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiSnVhbiBNZW5lbmRleiIsImV4cCI6MTc4MDM2NzM2NywiaXNzIjoiVGFza0Zsb3dQcm8iLCJhdWQiOiJUYXNrRmxvd1Byb1VzZXJzIn0.pCuKR98IqbYa9WUVG1Jr4aTtaQCDgYMovl9fFK166wI"
-}
-
-*/
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IAuthService, AuthService>();
@@ -155,14 +104,16 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Aplicar migraciones pendientes automaticamente al iniciar
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
 }
 
-// app.UseHttpsRedirection();
+// Swagger disponible en todos los entornos (incluyendo Production en Railway)
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
